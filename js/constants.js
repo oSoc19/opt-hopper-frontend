053ca5a96816f45b4d85fb705f6e04c31d801d62
Pietervdvn <pietervdvn@gmail.com>
2018-09-18 14:06:32 +0200

    Fixed geocoding

--- js/constants.js
+++ js/constants.js
@@ -4,8 +4,8 @@ const urls = {
     mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
     network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
     route: 'https://cycling-backend.anyways.eu/api',
-    geocoder: 'https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?'+
-                'access_token=${mapboxAccessCode}&proximity=5.5196%2c50.9612'+
+    geocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?`+
+                `access_token=${mapboxAccessCode}&proximity=5.5196%2c50.9612`+
                 'country=BE&'+
                 'bbox=5.3%2C50.70%2C5.7%2C51.1&'+
                 'limit=5&'+
