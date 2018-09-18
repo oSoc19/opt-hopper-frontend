a2103f9afa02a71609b63d7107816365949878d5
Pietervdvn <pietervdvn@gmail.com>
2018-09-18 14:04:10 +0200

    Updated geocoding

--- js/constants.js
+++ js/constants.js
@@ -4,7 +4,12 @@ const urls = {
     mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
     network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
     route: 'https://cycling-backend.anyways.eu/api',
-    geocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=${mapboxAccessCode}&proximity=50.861%2C4.356&country=BE&bbox=3.9784240723%2C50.6485897217%2C4.7282409668%2C51.0552073386&limit=5&types=place,locality,neighborhood,address,poi`,
+    geocoder: 'https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?'+
+                'access_token=${mapboxAccessCode}&proximity=5.5196%2c50.9612'+
+                'country=BE&'+
+                'bbox=5.3%2C50.70%2C5.7%2C51.1&'+
+                'limit=5&'+
+                'types=place,locality,neighborhood,address,poi',
     reverseGeocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0},{1}.json?limit=1&access_token=${mapboxAccessCode}`
 };
 
