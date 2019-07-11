1f801f28730c4622b7bec014e43da6e49d420d37
Fizz55 <neyt.kr@gmail.com>
2019-07-12 01:25:04 +0200

    Request multiple profiles

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -1,9 +1,10 @@
 
-const availableProfiles = [/*"bike",*/ "ebike"/*, "speedy"*/];
+const availableProfiles = ["bike", "ebike"/*, "speedy"*/];
 
 var profileConfigs = {
     "pedestrian": {
         backendName: "pedestrian",
+        routingProfile: "",
         routecolor: {
             backend: true,
             color: "#5a0449"
@@ -11,6 +12,7 @@ var profileConfigs = {
     },
     "bike": {
         backendName: "bicycle",
+        routingProfile: "",
         routecolor: {
             backend: true,
             color: "#315a33"
@@ -18,6 +20,12 @@ var profileConfigs = {
     },
     "ebike": {
         backendName: "ebike",
+        routingProfile: "&inBetweenOsmProfile=pedestrian&" +
+            "inBetweenSearchDistance=500&" +
+            "firstMileOsmProfile=ebike&" +
+            "firstMileSearchDistance=30000&" +
+            "lastMileOsmProfile=pedestrian&" +
+            "lastMileSearchDistance=10000",
         routecolor: {
             backend: true,
             color: "#2D495A"
@@ -25,6 +33,7 @@ var profileConfigs = {
     },
     "speedy": {
         backendName: "speedPedelec",
+        routingProfile: "",
         routecolor: {
             backend: true,
             color: "#6c6615"
@@ -45,28 +54,18 @@ function calculateAllRoutes(){
     console.log(inputData);
 
     for(let key in availableProfiles) {
-        // get the routing profile.
-        var profileConfig = profileConfigs[availableProfiles[key]];
-        let profile_url = profileConfig.backendName;
-        const prof = (profile_url === "" ? "" : `&profile=${profile_url}`);
         const dateParam = (isDeparture ? "&departure=" : "&arrival=") + encodeURIComponent(new Date(inputData.date).toISOString());
-        //const routingProfile = '&walksGeneratorDescription=' + encodeURIComponent('firstLastMile&default=osm&maxDistance=1000&profile=pedestrian&firstMile=osm&maxDistance=1000&profile=pedestrian&lastMile=osm&maxDistance=1000&profile=pedestrian');
-        //const routingProfile = '&walksGeneratorDescription=' + encodeURIComponent('osm&maxDistance=500&profile=pedestrian');
-        const routingProfile = "&inBetweenOsmProfile=pedestrian&" +
-            "inBetweenSearchDistance=500&" +
-            "firstMileOsmProfile=ebike&" +
-            "firstMileSearchDistance=30000&" +
-            "lastMileOsmProfile=pedestrian&" +
-            "lastMileSearchDistance=10000";
-
-        const url = `https://routing.anyways.eu/transitapi/journey?from=https%3A%2F%2Fwww.openstreetmap.org%2F%23map%3D19%2F${originS}&to=https%3A%2F%2Fwww.openstreetmap.org%2F%23map%3D19%2F${destinationS}${dateParam}${routingProfile}`;//${prof}`;
+        // get the routing profile.
         const profile = availableProfiles[key];
+        const routingProfile = profileConfigs[profile].routingProfile;
+
+        const url = `https://routing.anyways.eu/transitapi/journey?from=https%3A%2F%2Fwww.openstreetmap.org%2F%23map%3D19%2F${originS}&to=https%3A%2F%2Fwww.openstreetmap.org%2F%23map%3D19%2F${destinationS}${dateParam}${routingProfile}`;
 
         $.ajax({
             url: url,
             success: function (data) {
                 console.log(data);
-                displayRoute(profile, true, data.journeys[0]);
+                displayRoute(profile, profile === selectedProfile, data.journeys[0]);
                 fillItinerary(profile, inputData.fromName, inputData.toName, data.journeys[0]);
             },
             error: function (error) {
