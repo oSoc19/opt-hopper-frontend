92471317c23aa7b657b16e51ad28a6801fb4c302
Kristof <kristof.neyt@ugent.be>
2019-07-17 10:55:34 +0200

    Use Margot's mapstyle

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,10 +2,11 @@ let map;
 
 
 function loadMap(coords) { //long, lat
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsbGV0ZXJyYXMiLCJhIjoiY2pqeWJheGxhMGwxODNxbW1sb2UzMGo0aiJ9.Y5HiKm7qjB1vrX7NGTOofA';
+    //mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsbGV0ZXJyYXMiLCJhIjoiY2pqeWJheGxhMGwxODNxbW1sb2UzMGo0aiJ9.Y5HiKm7qjB1vrX7NGTOofA'; //anyways
+    mapboxgl.accessToken = 'pk.eyJ1IjoibXZlcmJzIiwiYSI6ImNqeTZ6c215MjBsdmozY21zMzEyNnZhNGcifQ.sRED4F6Fh59-wz90S0st4Q';  //Margot
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
+        style: 'mapbox://styles/mverbs/cjy6ztopn10dx1cpfxw5y0wz1',//mapbox://styles/mapbox/streets-v11',
         center: coords,
         zoom: 9
     });
